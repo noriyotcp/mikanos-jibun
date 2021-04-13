c45648ca0af39d44ac9164597ae905060229198a
noriyotcp <noriyo.akita@gmail.com>
2021-04-13 21:50:06 +0900

    5.1 文字を書いてみる (osbook_day05a)

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -9,6 +9,25 @@
 
 #include "frame_buffer_config.hpp"
 
+const uint8_t kFontA[16] = {
+    0b00000000, //
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b01111110, //  ******
+    0b01000010, //  *    *
+    0b01000010, //  *    *
+    0b01000010, //  *    *
+    0b11100111, // ***  ***
+    0b00000000, //
+    0b00000000, //
+};
+
 struct PixelColor {
   uint8_t r, g, b;
 };
@@ -52,6 +71,20 @@ public:
   }
 };
 
+void WriteAscii(PixelWriter &writer, int x, int y, char c,
+                const PixelColor &color) {
+  if (c != 'A') {
+    return;
+  }
+  for (size_t dy = 0; dy < 16; ++dy) {
+    for (size_t dx = 0; dx < 8; ++dx) {
+      if ((kFontA[dy] << dx) & 0x80u) {
+        writer.Write(x + dx, y + dy, color);
+      }
+    }
+  }
+}
+
 void *operator new(size_t size, void *buf) { return buf; }
 
 void operator delete(void *obj) noexcept {}
@@ -81,5 +114,7 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
       pixel_writer->Write(x, y, {0, 255, 0});
     }
   }
+  WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
+  WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
   while (1) __asm__("hlt");
 }
