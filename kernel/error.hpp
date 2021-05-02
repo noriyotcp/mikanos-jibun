fdfc64b9c3c07a29415a6c5f6a070b62d90ea1f5
noriyotcp <noriyo.akita@gmail.com>
2021-05-02 16:42:59 +0900

    6.4 ポーリングでマウス入力 (osbook_day06c)
    Update error.hpp
    エラーを返すときは MAKE_ERROR 関数を通す
    
    Update pci.hpp
    Fix ReadClassCode to return ClassCode
    Implement ReadClassCode, etc...
    Implement more functions in pci.cpp
    Update Makefile
    Add libcxx_support.cpp
    Add logger
    Add operator+= to Vector2D
    Extract to MouseCursor class
    
    Add _exit, getpid, and kill to newlib_support.c
    usb/ のほうで必要になるみたいだ
    
    Add register.hpp
    これも usb/ のほうで使うようだ
    
    Add .gitignore to kernel/
    
    kernel dir 配下に吐き出されるオブジェクトファイルなどは kernel/.gitignore で無視するようにした
    
    Update .gitignore
    .vscode だけを無視するようにした
    
    Create usb/ and implement files
    
    Add usb/setupdata.hpp
    uint8_t, uint16_t などが VS Code で undefined になる
    
    Add endpoint.hpp
    Add usb/arraymap.hpp
    Add usb/device.hpp
    Add usb/descriptor.hpp
    Add usb/classdriver and base.cpp and base.hpp
    Add hid.cpp and hid.hpp to usb/classdriver
    Add memory.cpp and memory.hpp to usb/
    Add keyboard.cpp and keyboard.hpp to usb/
    Add mouse.cpp and mouse.hpp
    Add usb/device.cpp
    Add usb/xhci, registers.cpp and registers.hpp
    Add xhci/context.hpp
    Add xhci/trb.cpp and trb.hpp
    Add ring.cpp and ring.hpp to xhci
    Add device.cpp and device.hpp to xhci
    Add port.cpp and port.hpp to xhci
    Add devmgr.cpp and devmgr.hpp to xhci
    Add speed.hpp to xhci
    Add xhci.cpp and xhci.hpp to xhci
    
    Implement mouse cursor move
    
    Add missing __cxa_pure_virtual
    Fix variable value_
    Update Makefile to add object files to OBJS
    Add missing WriteConfReg()
    Remove an extra arguments from ReadConfig()

--- kernel/error.hpp
+++ kernel/error.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <cstdio>
 
 class Error {
 public:
@@ -8,10 +9,60 @@ public:
     kSuccess,
     kFull,
     kEmpty,
-    kLastOfCode,
+    kNoEnoughMemory,
+    kIndexOutOfRange,
+    kHostControllerNotHalted,
+    kInvalidSlotID,
+    kPortNotConnected,
+    kInvalidEndpointNumber,
+    kTransferRingNotSet,
+    kAlreadyAllocated,
+    kNotImplemented,
+    kInvalidDescriptor,
+    kBufferTooSmall,
+    kUnknownDevice,
+    kNoCorrespondingSetupStage,
+    kTransferFailed,
+    kInvalidPhase,
+    kUnknownXHCISpeedID,
+    kNoWaiter,
+    kLastOfCode, // この列挙子は常に最後に配置する
   };
 
-  Error(Code code) : code_{code} {};
+private:
+  // <const char *, 20> を入れないと以下の警告が出る。ビルドはできる
+  // argument list for class template "std::__1::array" is missingC/C++(441)
+  // expression must have a constant value -- invalid type "<error-type>" for
+  // constant-expression evaluationC/C++(28)
+  static constexpr std::array<const char *, 20> code_names_{
+      "kSuccess",
+      "kFull",
+      "kEmpty",
+      "kNoEnoughMemory",
+      "kIndexOutOfRange",
+      "kHostControllerNotHalted",
+      "kInvalidSlotID",
+      "kPortNotConnected",
+      "kInvalidEndpointNumber",
+      "kTransferRingNotSet",
+      "kAlreadyAllocated",
+      "kNotImplemented",
+      "kInvalidDescriptor",
+      "kBufferTooSmall",
+      "kUnknownDevice",
+      "kNoCorrespondingSetupStage",
+      "kTransferFailed",
+      "kInvalidPhase",
+      "kUnknownXHCISpeedID",
+      "kNoWaiter",
+  };
+  static_assert(Error::Code::kLastOfCode == code_names_.size());
+
+public:
+  Error(Code code, const char *file, int line)
+      : code_{code}, line_{line}, file_{file} {};
+
+  Code Cause() const { return this->code_; }
 
   operator bool() const { return this->code_ != kSuccess; }
 
@@ -19,12 +70,19 @@ public:
     return code_names_[static_cast<int>(this->code_)];
   }
 
-private:
-  static constexpr std::array<const char *, 3> code_names_ = {
-      "kSuccess",
-      "kFull",
-      "kEmpty",
-  };
+  const char *File() const { return this->file_; }
 
+  int Line() const { return this->line_; }
+
+private:
   Code code_;
+  int line_;
+  const char *file_;
+};
+
+#define MAKE_ERROR(code) Error((code), __FILE__, __LINE__)
+
+template <class T> struct WithError {
+  T value;
+  Error error;
 };
