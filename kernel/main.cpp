6947caa4801341b72e7344e218e818ea28797f42
noriyotcp <noriyo.akita@gmail.com>
2021-08-30 19:50:36 +0900

    12.6 カーソル (osbook_day12f)
    Add DrawTextCursor()
    
    カーソルを描いたり消したりする
    visible を真にするとカーソルを表示する。偽にすると非表示にする
    消すと言っても白で塗りつぶすだけ
    
    Use DrawTextCursor() in textbox
    
    Add Timer with timeout over 0.5
    
    kTimerFreq は1秒間に tick_ が増える数。1秒間の tick_ の変化量を計算できる
    timer_manager がタイマ割り込みハンドラの中で使われるので、割り込みハンドラ外から操作するときは一時的に割り込みを禁止する必要がある
    
    カーソル用タイマがタイムアウトしたときの処理
    
    msg.arg.timer.value が期待したとおりのものであれば割り込み処理を禁止した上で 0.5 秒後にタイムアウトするタイマーを追加する
    textbox_cursor_visible を反転し、DrawTextCursor に渡す。これで点滅を実現する

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -53,16 +53,12 @@ void InitializeMainWindow() {
   main_window = std::make_shared<Window>(160, 52, screen_config.pixel_format);
   DrawWindow(*main_window->Writer(), "Hello Window");
 
-  main_window_layer_id = layer_manager->NewLayer()
-                             .SetWindow(main_window)
-                             .SetDraggable(true)
-                             .Move({300, 100})
-                             .ID();
+  main_window_layer_id =
+      layer_manager->NewLayer().SetWindow(main_window).SetDraggable(true).Move({300, 100}).ID();
 
   layer_manager->UpDown(main_window_layer_id, std::numeric_limits<int>::max());
 }
 
-// #@@range_begin(init_textwin)
 std::shared_ptr<Window> text_window;
 unsigned int text_window_layer_id;
 void InitializeTextWindow() {
@@ -78,10 +74,15 @@ void InitializeTextWindow() {
 
   layer_manager->UpDown(text_window_layer_id, std::numeric_limits<int>::max());
 }
-// #@@range_end(init_textwin)
 
-// #@@range_begin(input_textwin)
 int text_window_index;
+
+void DrawTextCursor(bool visible) {
+  const auto color = visible ? ToColor(0) : ToColor(0xffffff);
+  const auto pos = Vector2D<int>{8 + 8 * text_window_index, 24 + 5};
+  FillRectangle(*text_window->Writer(), pos, {7, 15}, color);
+}
+
 void InputTextWindow(char c) {
   if (c == 0) {
     return;
@@ -89,28 +90,29 @@ void InputTextWindow(char c) {
 
   auto pos = []() { return Vector2D<int>{8 + 8 * text_window_index, 24 + 6}; };
 
-  const int max_chars = (text_window->Width() - 16) / 8;
-  if (c == '\b' && text_window_index > 0) {
+  const int max_chars = (text_window->Width() - 16) / 8 - 1;
+  if (c == '\b' && text_window_index > 0) { // Delete key を押したときの挙動
+    DrawTextCursor(false);
     --text_window_index;
     FillRectangle(*text_window->Writer(), pos(), {8, 16}, ToColor(0xffffff));
+    DrawTextCursor(true);
   } else if (c >= ' ' && text_window_index < max_chars) {
+    DrawTextCursor(false);
     WriteAscii(*text_window->Writer(), pos(), c, ToColor(0));
     ++text_window_index;
+    DrawTextCursor(true);
   }
 
   layer_manager->Draw(text_window_layer_id);
 }
-// #@@range_end(input_textwin)
 
 std::deque<Message> *main_queue;
 
 alignas(16) uint8_t kernel_main_stack[1024 * 1024];
 
 // #@@range_begin(main_function)
-extern "C" void
-KernelMainNewStack(const FrameBufferConfig &frame_buffer_config_ref,
-                   const MemoryMap &memory_map_ref,
-                   const acpi::RSDP &acpi_table) {
+extern "C" void KernelMainNewStack(const FrameBufferConfig &frame_buffer_config_ref,
+                                   const MemoryMap &memory_map_ref, const acpi::RSDP &acpi_table) {
   MemoryMap memory_map{memory_map_ref};
 
   InitializeGraphics(frame_buffer_config_ref);
@@ -140,23 +142,24 @@ KernelMainNewStack(const FrameBufferConfig &frame_buffer_config_ref,
 
   InitializeKeyboard(*main_queue);
 
-  timer_manager->AddTimer(Timer(200, 2));
-  timer_manager->AddTimer(Timer(600, -1));
+  const int kTextboxCursorTimer = 1;
+  const int kTimer05sec = static_cast<int>(kTimerFreq * 0.5);
+  __asm__("cli");
+  timer_manager->AddTimer(Timer{kTimer05sec, kTextboxCursorTimer});
+  __asm__("sti");
+  bool textbox_cursor_visible = false;
 
   char str[128];
 
   while (true) {
-    // #@@range_begin(show_tick)
     __asm__("cli");
-      const auto tick = timer_manager->CurrentTick();
+    const auto tick = timer_manager->CurrentTick();
     __asm__("sti");
 
     sprintf(str, "%010lu", tick);
-    FillRectangle(*main_window->Writer(), {24, 28}, {8 * 10, 16},
-                  {0xc6, 0xc6, 0xc6});
+    FillRectangle(*main_window->Writer(), {24, 28}, {8 * 10, 16}, {0xc6, 0xc6, 0xc6});
     WriteString(*main_window->Writer(), {24, 28}, str, {0, 0, 0});
     layer_manager->Draw(main_window_layer_id);
-    // #@@range_end(show_tick)
 
     __asm__("cli");
     if (main_queue->size() == 0) {
@@ -173,6 +176,14 @@ KernelMainNewStack(const FrameBufferConfig &frame_buffer_config_ref,
       usb::xhci::ProcessEvents();
       break;
     case Message::kTimerTimeout:
+      if (msg.arg.timer.value == kTextboxCursorTimer) {
+        __asm__("cli");
+        timer_manager->AddTimer(Timer{msg.arg.timer.timeout + kTimer05sec, kTextboxCursorTimer});
+        __asm__("sti");
+        textbox_cursor_visible = !textbox_cursor_visible;
+        DrawTextCursor(textbox_cursor_visible);
+        layer_manager->Draw(text_window_layer_id);
+      }
       break;
     case Message::kKeyPush:
       InputTextWindow(msg.arg.keyboard.ascii);
